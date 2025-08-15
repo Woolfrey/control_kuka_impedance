c3dca3b810b1ab8f3ba9713a063b499fbee5e92f
Jon <jonathan.woolfrey@gmail.com>
2025-08-15 09:49:34 +0100

    Modified to use different models

--- launch_follow_transform.sh
+++ launch_follow_transform.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Argument (default to iiwa14)
+MODEL=${1:-iiwa14}
+
 # Get install paths using ros2 pkg prefix
-PKG_PREFIX=$(ros2 pkg prefix kuka_impedance_control)
-CONFIG_DIR="${PKG_PREFIX}/share/kuka_impedance_control/config"
+PKG_PREFIX=$(ros2 pkg prefix serial_link_impedance_control)
+CONFIG_DIR="${PKG_PREFIX}/share/serial_link_impedance_control/config"
 
 # Open a new terminal and run the action server
-gnome-terminal -- bash -c "ros2 launch kuka_impedance_control follow_transform.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch serial_link_impedance_control follow_transform.py model:=${MODEL}; exec bash"
 
 # Wait for the server to start
 sleep 1
@@ -13,6 +16,5 @@ sleep 1
 # Run the client with installed parameter file paths
 ros2 run serial_link_action_client follow_transform_client \
   --ros-args \
-  --params-file "${CONFIG_DIR}/iiwa_joint_configurations.yaml" \
+  --params-file "${CONFIG_DIR}/${MODEL}/joint_configurations.yaml" \
   --params-file "${CONFIG_DIR}/tolerances.yaml"
-
--- launch_trajectory_tracking.sh
+++ launch_trajectory_tracking.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Argument (default to iiwa14)
+MODEL=${1:-iiwa14}
+
 # Get install paths using ros2 pkg prefix
-PKG_PREFIX=$(ros2 pkg prefix kuka_impedance_control)
-CONFIG_DIR="${PKG_PREFIX}/share/kuka_impedance_control/config"
+PKG_PREFIX=$(ros2 pkg prefix serial_link_impedance_control)
+CONFIG_DIR="${PKG_PREFIX}/share/serial_link_impedance_control/config"
 
 # Open a new terminal and run the action server
-gnome-terminal -- bash -c "ros2 launch kuka_impedance_control trajectory_tracking.py; exec bash"
+gnome-terminal -- bash -c "ros2 launch serial_link_impedance_control trajectory_tracking.py model:=${MODEL}; exec bash"
 
 # Wait for the server to start
 sleep 1
@@ -13,7 +16,6 @@ sleep 1
 # Run the client with installed parameter file paths
 ros2 run serial_link_action_client trajectory_tracking_client \
   --ros-args \
-  --params-file "${CONFIG_DIR}/iiwa_endpoint_poses.yaml" \
-  --params-file "${CONFIG_DIR}/iiwa_joint_configurations.yaml" \
+  --params-file "${CONFIG_DIR}/${MODEL}/endpoint_poses.yaml" \
+  --params-file "${CONFIG_DIR}/${MODEL}/joint_configurations.yaml" \
   --params-file "${CONFIG_DIR}/tolerances.yaml"
-
